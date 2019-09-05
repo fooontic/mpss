c0b4143f7ac5f74914afbc2a6353073c60d3964f
fooontic <fooontic@gmail.com>
2019-09-05 10:20:39 +0300

    fix js file

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -8,7 +8,7 @@ module.exports = {
 		{ elem: 'css', url: 'index.min.css' },
 		{ elem: 'js', url: 'https://code.jquery.com/jquery-3.2.1.js' },
 	],
-	scripts: [{ elem: 'js', url: 'index.js' }],
+	scripts: [{ elem: 'js', url: 'index.min.js' }],
 	mix: {
 		block: 'theme',
 		mods: { color: 'gpn-default', control: 'gpn-default', space: 'default', breakpoint: 'default', space: 'default', size: 'gpn-default', gap: 'medium', font: 'gpn-default' }
